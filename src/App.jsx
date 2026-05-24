d098909013bb56b5a989f1076583bc730ff32e57
Олександр Ніценко <niroshi.0@MacBook-Air-Oleksandr-2.local>
2026-05-24 18:24:05 +0200

    Adding Routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import './App.css'
 
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
+import AboutPage from './pages/AboutPage'
+import ContactPage from './pages/ContactPage'
+import SkillsPage from './pages/SkillsPage'
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/skills" element={<SkillsPage />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
     </main>
   )
