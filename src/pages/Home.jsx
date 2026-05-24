10ba98ec07d3b23c05cf58665e9855a41bdfa75a
Олександр Ніценко <niroshi.0@MacBook-Air-Oleksandr-2.local>
2026-05-24 18:09:53 +0200

    Install router and start work with pages; Adding in pages: Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import Hero from '../components/Hero'
+import About from '../components/About'
+import Skills from '../components/Skills'
+import Contact from '../components/Contact'
+import Footer from '../components/Footer'
+
+function Home() {
+  return (
+    <>
+      <Hero />
+      <About />
+      <Skills />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default Home
